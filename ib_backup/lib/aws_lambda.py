41130ce4e789a125bcee82fb23d008b37993b8d2
Noah Huppert <contact@noahh.io>
2018-07-31 17:06:09 -0400

    ib backup: merged aws lambda helper function

---
+++ ib_backup/lib/aws_lambda.py
@@ -0,0 +1,15 @@
+import json
+from typing import Dict
+
+import boto3
+
+def invoke_lambda(function_name: str, data: Dict[str, object]):
+    """ Invokes a Lambda function
+    Args:
+        - function_name: Name of Lambda function to invoke
+        - data: Any to provide to invoked function, must be JSON seralizable
+    """
+    lambda_client = boto3.client('lambda')
+
+    resp = lambda_client.invoke(FunctionName=function_name, InvocationType='Event', LogType='Trail',
+                                Payload=json.dumps(data))
